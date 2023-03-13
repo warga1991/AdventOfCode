f752bc443c43990c6a5b26da2723ad01fa868ef7
warga1991 <108489173+warga1991@users.noreply.github.com>
2023-03-13 15:35:28 +0100

    task2 done

--- day4/day4.py
+++ day4/day4.py
@@ -5,13 +5,12 @@ from read_in import read_lines_in
 raw_input_array = read_lines_in(r"C:\Users\Donát\Documents\GitHub\AdventOfCode\day4\input.txt")
 working_pairs_processed = list(map(lambda x: x.split(','), raw_input_array))
 
-overlap_cnt = 0
+not_overlaping_cnt = 0
 for working_pair in working_pairs_processed:
-    first_worker = working_pair[0].split('-')
-    second_worker = working_pair[1].split('-')
-    first_inside_second = int(first_worker[0]) >= int(second_worker[0]) and int(first_worker[1]) <= int(second_worker[1])
-    second_inside_first = int(first_worker[0]) <= int(second_worker[0]) and int(first_worker[1]) >= int(second_worker[1])
-    if first_inside_second or second_inside_first:
-        overlap_cnt += 1
-
-print(overlap_cnt)
+    first_worker = [int(i) for i in working_pair[0].split('-')]
+    second_worker = [int(i) for i in working_pair[1].split('-')] 
+    first_worker_first_task, first_worker_second_task = first_worker[0], first_worker[1]
+    second_worker_first_task, second_worker_second_task = second_worker[0], second_worker[1]
+    if first_worker_first_task > second_worker_second_task or second_worker_first_task > first_worker_second_task:
+        not_overlaping_cnt += 1
+print(len(working_pairs_processed) - not_overlaping_cnt)
